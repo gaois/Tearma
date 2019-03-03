8bc66a592319be034a9be34a71967ce787b3ab09
Michal Boleslav Měchura <valselob@gmail.com>
2019-03-03 18:30:13 +0100

    email addresses in enquiry form

--- TearmaWeb/Controllers/AskController.cs
+++ TearmaWeb/Controllers/AskController.cs
@@ -43,8 +43,7 @@ namespace TearmaWeb.Controllers {
 				if(name!="") html+="AINM:<br/>"+name+"<br/><br/>";
 				if(email!="") html+="SEOLADH RÍOMHPHOIST:<br/>"+email+"<br/><br/>";
 				if(phone!="") html+="UIMHIR GHUTHÁIN:<br/>"+phone+"<br/><br/>";
-                //SendEmail("tearmai@forasnagaeilge.ie", subject, html);
-				SendEmail("valselob@gmail.com", subject, html);
+                SendEmail("tearmai@forasnagaeilge.ie", subject, html);
 			} else {
 				model.mode="error";
 			}
@@ -54,9 +53,10 @@ namespace TearmaWeb.Controllers {
 
 		public static void SendEmail(string to, string subject, string body)
 		{
+			//System.Net.Mail.MailMessage msg=new System.Net.Mail.MailMessage("noreply@tearma.ie", to, subject, body);
 			System.Net.Mail.MailMessage msg=new System.Net.Mail.MailMessage("noreply@tearma.ie", to, subject, body);
 			msg.IsBodyHtml=true;
-			//msg.Bcc="eolas@tearma.ie";
+			msg.Bcc.Add("eolas@tearma.ie");
 			msg.BodyEncoding=System.Text.Encoding.UTF8;
 			System.Net.Mail.SmtpClient client=new System.Net.Mail.SmtpClient("localhost");
 			client.Send(msg);
